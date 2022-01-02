3c01868373925ce4818fcd7d2e6d549ecdc0aa97
wangxa <48665020+wangxa97@users.noreply.github.com>
2022-01-02 14:34:59 +0800

    pull commite

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("merge conflict from hot-fix");
         System.out.println("merge conflict from hot-fix 2");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
