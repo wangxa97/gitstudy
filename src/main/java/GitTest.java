a9852dcb21f327a4712e7ed914744de3f49ca2f7
WXA <wyljndx@163.com>
2022-01-02 14:24:12 +0800

    push commit master

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
         System.out.println("hello git1");
         System.out.println("hot fix");
         System.out.println("merge conflict from hot-fix");
+        System.out.println("merge conflict from hot-fix 2");
+        System.out.println("push test");
     }
 }
